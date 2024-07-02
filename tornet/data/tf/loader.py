dea4a030a90d97e2dc868aecdbdf578debb7ac3e
Mark Veillette <mark.veillette@ll.mit.edu>
2024-07-02 16:42:00 -0400

    fixed tf loader

--- tornet/data/tf/loader.py
+++ tornet/data/tf/loader.py
@@ -163,8 +163,11 @@ def preproc(ds: tf.data.Dataset,
     if weights:
         ds = ds.map(lambda x,y:  pp.compute_sample_weight(x,y,**weights, backend=tf) )
     
-    # select keys for input
-    if select_keys is not None:
-        ds = ds.map(lambda xy: (pp.select_keys(xy[0],keys=select_keys)+xy[1:]))
+        # select keys for input
+        if select_keys is not None:
+            ds = ds.map(lambda x,y,w: (pp.select_keys(x,keys=select_keys),y,w))
+    else:
+        if select_keys is not None:
+            ds = ds.map(lambda x,y: (pp.select_keys(x,keys=select_keys),y))
 
     return ds
